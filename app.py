b575a7616ba56fe942678ce1c3d90377d318cc61
vishnu-kannppan-palaniappan <vishnu_kannappan_palaniappan@s2023.ssts.edu.sg>
2026-01-21 11:16:28 +0800

    add 3 pages for main functions

--- app.py
+++ app.py
@@ -16,26 +16,27 @@ def save_users(users):
 
 if "user" not in st.session_state:
     st.session_state.user = None
-if "mode" not in st.session_state:
-    st.session_state.mode = None
+if "page" not in st.session_state:
+    st.session_state.page = "auth"
 
 def auth_page():
     st.title("Sign In / Sign Up")
-    st.session_state.mode = st.radio("Mode", ["Sign In", "Sign Up"])
+    mode = st.radio("Mode", ["Sign In", "Sign Up"])
 
     with st.form(key="auth_form"):
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
-        submit = st.form_submit_button(st.session_state.mode)
+        submit = st.form_submit_button(mode)
 
         if submit:
             users = load_users()
-            if st.session_state.mode == "Sign In":
+            if mode == "Sign In":
                 if username in users and users[username] == password:
                     st.session_state.user = username
+                    st.session_state.page = "home"
                 else:
                     st.error("Invalid login")
-            else:  # Sign Up
+            else:
                 if username in users:
                     st.error("User already exists")
                 else:
@@ -47,12 +48,56 @@ def home_page():
     st.title("Home")
     st.write(f"Welcome, {st.session_state.user}")
 
+    # home buttons to go to features
+    col1, col2, col3 = st.columns(3)
+    if col1.button("Feature 1"):
+        st.session_state.page = "feature1"
+    if col2.button("Feature 2"):
+        st.session_state.page = "feature2"
+    if col3.button("Feature 3"):
+        st.session_state.page = "feature3"
+
     if st.button("Log out"):
         st.session_state.user = None
+        st.session_state.page = "auth"
+
+def feature1_page():
+    st.title("Feature 1")
+    st.write("This is feature 1")
+    if st.button("Back to Home"):
+        st.session_state.page = "home"
+
+def feature2_page():
+    st.title("Feature 2")
+    st.write("This is feature 2")
+    if st.button("Back to Home"):
+        st.session_state.page = "home"
+
+def feature3_page():
+    st.title("Feature 3")
+    st.write("This is feature 3")
+    if st.button("Back to Home"):
+        st.session_state.page = "home"
+
+if st.session_state.user:
+    page = st.sidebar.selectbox("Navigation", ["Home", "Feature 1", "Feature 2", "Feature 3"])
+    if page == "Home":
+        st.session_state.page = "home"
+    elif page == "Feature 1":
+        st.session_state.page = "feature1"
+    elif page == "Feature 2":
+        st.session_state.page = "feature2"
+    elif page == "Feature 3":
+        st.session_state.page = "feature3"
 
 if st.session_state.user is None:
     auth_page()
 else:
-    home_page()
-
-
+    if st.session_state.page == "home":
+        home_page()
+    elif st.session_state.page == "feature1":
+        feature1_page()
+    elif st.session_state.page == "feature2":
+        feature2_page()
+    elif st.session_state.page == "feature3":
+        feature3_page()
