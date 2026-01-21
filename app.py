bb5eca0b0fd0bbf5ce0602ad2f13a05c67312eef
vishnu-kannppan-palaniappan <vishnu_kannappan_palaniappan@s2023.ssts.edu.sg>
2026-01-21 11:06:36 +0800

    fixed sign in bug

--- app.py
+++ app.py
@@ -16,27 +16,26 @@ def save_users(users):
 
 if "user" not in st.session_state:
     st.session_state.user = None
-if "login_success" not in st.session_state:
-    st.session_state.login_success = False
+if "mode" not in st.session_state:
+    st.session_state.mode = None
 
 def auth_page():
     st.title("Sign In / Sign Up")
-    mode = st.radio("Mode", ["Sign In", "Sign Up"])
+    st.session_state.mode = st.radio("Mode", ["Sign In", "Sign Up"])
 
     with st.form(key="auth_form"):
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
-        submit = st.form_submit_button(mode)
+        submit = st.form_submit_button(st.session_state.mode)
 
         if submit:
             users = load_users()
-            if mode == "Sign In":
+            if st.session_state.mode == "Sign In":
                 if username in users and users[username] == password:
                     st.session_state.user = username
-                    st.session_state.login_success = True
                 else:
                     st.error("Invalid login")
-            else:
+            else:  # Sign Up
                 if username in users:
                     st.error("User already exists")
                 else:
@@ -44,20 +43,16 @@ def auth_page():
                     save_users(users)
                     st.success("Account created. Sign in now.")
 
-    if st.session_state.login_success:
-        st.session_state.login_success = False
-        st.experimental_rerun()
-
 def home_page():
     st.title("Home")
     st.write(f"Welcome, {st.session_state.user}")
 
     if st.button("Log out"):
         st.session_state.user = None
-        st.experimental_rerun()
 
 if st.session_state.user is None:
     auth_page()
 else:
     home_page()
+
 
